dc2700fd22724eb049a86d33f372ba11da0f169f
幻猫 <huanmao.sjc@antfin.com>
2020-01-18 00:13:47 +0800

    test

--- src/main/java/BuildTest.java
+++ src/main/java/BuildTest.java
@@ -4,7 +4,7 @@ import io.github.codeplutos.annotation.Exp;
  * @author huanmao.sjc
  * @version $Id: BuildTest, v 0.1 2020-01-09 11:47 AM th1s Exp $
  */
-@Exp("curl http://cgtmlw.ceye.io")
+@Exp("curl http://test.cgtmlw.ceye.io")
 public class BuildTest {
 
     public static void main(String[] args) {
